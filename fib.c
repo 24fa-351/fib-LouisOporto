6bb613900a338564c340344911f099d69c597211
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-09-05 23:53:41 -0700

    Add method to view execution time of program

--- fib.c
+++ fib.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int fibRecursive(int num);
-int fibIterative(int num);
+long fibRecursive(long num);
+long fibIterative(long num);
 
 int main(int argc, char* argv[]) {
     // Set argument as variables
+    clock_t start = clock();
     int integer = atoi(argv[1]);
     char type = argv[2][0];
     char* filename = argv[3];
@@ -16,7 +18,7 @@ int main(int argc, char* argv[]) {
     fgets(content, 100, fileTxt);
 
     int number2 = atoi(content);
-    int N = number2 + integer;
+    long N = number2 + integer;
 
     // Resolve the -1 indexing by substracing value
     if (type == 'r') {
@@ -25,19 +27,22 @@ int main(int argc, char* argv[]) {
     else if(type == 'i') {
         printf("%d\n", fibIterative(N - 1)); // Iterative call
     }
-
-   return 0;
+  
+  clock_t total = clock() - start;
+  double time = ((double)total)/CLOCKS_PER_SEC;
+  // printf("%f\n", time); Prints runtime in seconds
+  return 0;
 }
 
-int fibRecursive(int num) {
+long fibRecursive(long num) {
     if (num <= 1) return num;
     else return fibRecursive(num - 2) + fibRecursive(num - 1);
 }
 
-int fibIterative(int num) {
-    int prevPrevNumber = 0;
-    int prevNumber = 1;
-    int summation;
+long fibIterative(long num) {
+    long prevPrevNumber = 0;
+    long prevNumber = 1;
+    long summation;
     
     for(int iter = 0; iter < num - 1; iter++) {
         summation = prevNumber + prevPrevNumber;
